a8a9bd34fcbd47419953e13a902a5aa88e8ddded
senseiwells <wellsm488@gmail.com>
2026-09-16 12:48:38 +0100

    chore: Bump arcade and let sp worlds bypass permissions

--- src/main/kotlin/me/senseiwells/kursive/common/utils/PermissionUtils.kt
+++ src/main/kotlin/me/senseiwells/kursive/common/utils/PermissionUtils.kt
@@ -2,11 +2,19 @@ package me.senseiwells.kursive.common.utils
 
 import me.senseiwells.kursive.common.permissions.KursivePermission
 import me.senseiwells.kursive.server.KursiveServer
+import net.casual.arcade.utils.player.hasPermission
+import net.casual.arcade.utils.player.server
 import net.fabricmc.fabric.api.permission.v1.PermissionContextOwner
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.server.permissions.PermissionLevel
 
 @Suppress("UnstableApiUsage")
 fun PermissionContextOwner.checkPermission(permission: KursivePermission): Boolean {
     if (!KursiveServer.config.requirePermissions) {
+        return true
+    }
+
+    if (this is ServerPlayer && this.server.isSingleplayer && this.hasPermission(PermissionLevel.GAMEMASTERS)) {
         return true
     }
 
